65f6d71da142dec2cccfa462707021f10aea1526
Ryan Cook <ryencook@gmail.com>
2017-08-27 16:43:17 -0400

    Update Pointers

--- Pointers/main.cpp
+++ Pointers/main.cpp
@@ -16,5 +16,19 @@ int main() {
 	cout << "Value of *ip variable: ";
 	cout << *ip << endl;
 
+	// Reference variables, any change made to the original will affect the child
+	int i;
+	int& j = i;
+
+	int input;
+
+	cout << "Enter in a value: " << flush;
+	cin >> input;
+
+	i = input;
+
+	cout <<  "Value of i: " << j << endl;
+	cout << "Value of i reference: " << i << endl;
+
 	return 0;
 }
