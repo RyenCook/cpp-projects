d2639327776b0d5c66569d48cc43e9520c747ea9
Ryan Cook <ryencook@gmail.com>
2017-10-13 08:57:00 -0400

    Update Banking3

--- programs/banking3/bank.cpp
+++ programs/banking3/bank.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <vector>
 #include "bankaccount.h"
 #include "bank.h"
 
@@ -16,4 +17,15 @@ Bank::Bank(std::string t_name, BankAccount& t_account)
 {
 	m_name = t_name;
 	m_account = t_account;
+}
+
+void Bank::addAccount(BankAccount& t_account)
+{
+	m_accounts.push_back(t_account);
+}
+
+std::string getAccount(int i)
+{
+	m_accounts.pushback(t_account);;
+	return ();
 }
--- programs/banking3/bank.h
+++ programs/banking3/bank.h
@@ -2,16 +2,20 @@
 #define BANK_H_
 
 #include <string>
+#include <vector>
 #include "bankaccount.h"
 
 class Bank
 {
 	public:
 		std::string m_name;
-		BankAccount m_account; // Vector??
+		BankAccount m_account;
+		std::vector<BankAccount> m_accounts;// Vector??
 		Bank();
 		Bank(std::string t_name);
 		Bank(std::string t_name, BankAccount& t_account);
+		void addAccount(BankAccount& t_account);
+		std::string getAccount(int i);
 };
 
 #endif /* BANK_H_ */
