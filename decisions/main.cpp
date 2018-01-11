8fc0c55c077296437e974e6beddd56d4116963d8
Ryan Cook <ryencook@gmail.com>
2018-01-11 12:07:00 -0500

    gg=G

--- decisions/main.cpp
+++ decisions/main.cpp
@@ -6,42 +6,42 @@ using namespace std;
  */
 
 int main() {
-	int input;
-	cout << "Input a value: " << endl;
-	cin >> input;
+    int input;
+    cout << "Input a value: " << endl;
+    cin >> input;
 
-	if(input%2 == 0) {
-		cout << "The value is even." << endl;
-	}else{
-		cout << "The value is odd." << endl;
-	}
+    if(input%2 == 0) {
+        cout << "The value is even." << endl;
+    }else{
+        cout << "The value is odd." << endl;
+    }
 
-	char grade;
-	cout << "Input a grade: " << endl;
-	cin >> grade;
+    char grade;
+    cout << "Input a grade: " << endl;
+    cin >> grade;
 
-	switch(grade) {
-		case 'A':
-			cout << "Excellent!" << endl;
-			break;
-		case 'B':
-			cout << "Well done." << endl;
-			break;
-		case 'C':
-			cout << "You passed." << endl;
-			break;
-		case 'D':
-			cout << "NC" << endl;
-			break;
-		case 'F':
-			cout << "NC" << endl;
-			break;
-		default:
-			cout << "Invalid grade" << endl;
-			break;
-	}
+    switch(grade) {
+        case 'A':
+            cout << "Excellent!" << endl;
+            break;
+        case 'B':
+            cout << "Well done." << endl;
+            break;
+        case 'C':
+            cout << "You passed." << endl;
+            break;
+        case 'D':
+            cout << "NC" << endl;
+            break;
+        case 'F':
+            cout << "NC" << endl;
+            break;
+        default:
+            cout << "Invalid grade" << endl;
+            break;
+    }
 
-	cout << "Your grade is " << grade << endl;
+    cout << "Your grade is " << grade << endl;
 
-	return 0;
+    return 0;
 }
